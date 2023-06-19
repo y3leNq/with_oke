cbae94f3257611c2583d8d092dc4723103f49b16
aono yuto <y3lenq@gmail.com>
2023-06-20 01:33:00 +0900

    Fix: 新規登録のルーティングとビューファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get 'signup', to: 'users#new', as: :signup
+  post 'signup', to: 'users#create'
   get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy', as: :logout
-
-  resources :users, only: %i[new create]
 end
