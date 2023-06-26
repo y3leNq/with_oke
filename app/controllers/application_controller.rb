f5ca8314ed245a71a6824537c8b65e95481a01fb
aono yuto <y3lenq@gmail.com>
2023-06-27 00:07:06 +0900

    Resolved merge conflict in ja.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
   add_flash_types :success, :info, :warning, :danger
+
+  def not_authenticated
+    redirect_to login_path, danger: (t 'defaults.require_login')
+  end
 end
