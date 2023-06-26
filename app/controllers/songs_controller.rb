98f5fbb6a7eecb7879718cc6b66c03e78c619a98
aono yuto <y3lenq@gmail.com>
2023-06-26 16:56:09 +0900

    Add: songsコントローラーに楽曲を作成するコードを追加

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,30 @@
 class SongsController < ApplicationController
   def index; end
 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to songs_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def search
     if params[:search].present?
       @songs = Song.search(params[:search])
     else
       @songs = []
     end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:artist, :title)
   end
 end
