dab1dd25bdf12fa4b3faf3e6de0c7a084a59a45b
aono yuto <y3lenq@gmail.com>
2023-07-02 17:57:04 +0900

    Fix: songsデータベースに同一の楽曲が存在するときはバリデーションエラーが発生するよエラーが発生するように修正

--- app/models/song.rb
+++ app/models/song.rb
@@ -16,6 +16,7 @@ class Song < ApplicationRecord
   has_many :playlists, through: :playlist_songs
 
   validates :artist, :title, presence: true
+  validate :exist_song
 
   def self.search(query)
     ITunesSearchAPI.search(term: query, media: "music", country: 'jp', limit: 2)
@@ -23,5 +24,14 @@ class Song < ApplicationRecord
 
   def self.lookup(query)
     ITunesSearchAPI.lookup(id: query, country: 'jp')
+  end
+
+  def exist_song
+    if Song.exists?(title: title, artist: artist)
+      errors.add(:base, 'この楽曲は存在しています')
+      false
+    else
+      true
+    end
   end
 end
