1a541b59a7d6fc3e99617c36a3fb9efa1d278d15
aono yuto <y3lenq@gmail.com>
2023-08-03 18:34:20 +0900

    Add: プレビュー機能の実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,17 @@
+document.body.addEventListener('click', function(event) {
+  const playIcon = event.target.closest('.play-icon');
+  if (!playIcon) return;
+
+  const audioId = 'audio-' + playIcon.dataset.id;
+  const audioPlayer = document.getElementById(audioId);
+
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playIcon.classList.remove('fa-circle-play');
+    playIcon.classList.add('fa-circle-pause');
+  } else {
+    audioPlayer.pause();
+    playIcon.classList.remove('fa-circle-pause');
+    playIcon.classList.add('fa-circle-play');
+  }
+});
