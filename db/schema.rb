3bccb25a0cafb8e9798b55d3932ff154c9646049
aono yuto <y3lenq@gmail.com>
2023-08-04 20:40:40 +0900

    Fix: bundle exec rubocop -a

--- db/schema.rb
+++ db/schema.rb
@@ -10,78 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_152245) do
+ActiveRecord::Schema[7.0].define(version: 20_230_803_152_245) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "authentications", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "provider", null: false
-    t.string "uid", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+  create_table 'authentications', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.string 'provider', null: false
+    t.string 'uid', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[provider uid], name: 'index_authentications_on_provider_and_uid'
   end
 
-  create_table "playlist_songs", force: :cascade do |t|
-    t.bigint "playlist_id", null: false
-    t.bigint "song_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "key", default: 0, null: false
-    t.index ["playlist_id", "song_id"], name: "index_playlist_songs_on_playlist_id_and_song_id", unique: true
-    t.index ["playlist_id"], name: "index_playlist_songs_on_playlist_id"
-    t.index ["song_id"], name: "index_playlist_songs_on_song_id"
+  create_table 'playlist_songs', force: :cascade do |t|
+    t.bigint 'playlist_id', null: false
+    t.bigint 'song_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'key', default: 0, null: false
+    t.index %w[playlist_id song_id], name: 'index_playlist_songs_on_playlist_id_and_song_id', unique: true
+    t.index ['playlist_id'], name: 'index_playlist_songs_on_playlist_id'
+    t.index ['song_id'], name: 'index_playlist_songs_on_song_id'
   end
 
-  create_table "playlists", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_playlists_on_user_id"
+  create_table 'playlists', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'name', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_playlists_on_user_id'
   end
 
-  create_table "scores", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "song_id", null: false
-    t.integer "key", default: 0, null: false
-    t.decimal "score", precision: 5, scale: 3, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["song_id"], name: "index_scores_on_song_id"
-    t.index ["user_id"], name: "index_scores_on_user_id"
+  create_table 'scores', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'song_id', null: false
+    t.integer 'key', default: 0, null: false
+    t.decimal 'score', precision: 5, scale: 3, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['song_id'], name: 'index_scores_on_song_id'
+    t.index ['user_id'], name: 'index_scores_on_user_id'
   end
 
-  create_table "songs", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "artist", null: false
-    t.string "preview_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'songs', force: :cascade do |t|
+    t.string 'title', null: false
+    t.string 'artist', null: false
+    t.string 'preview_url'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
-    t.string "reset_password_token"
-    t.datetime "reset_password_token_expires_at"
-    t.datetime "reset_password_email_sent_at"
-    t.integer "access_count_to_reset_password_page", default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'email', null: false
+    t.string 'crypted_password'
+    t.string 'salt'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'remember_me_token'
+    t.datetime 'remember_me_token_expires_at'
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_token_expires_at'
+    t.datetime 'reset_password_email_sent_at'
+    t.integer 'access_count_to_reset_password_page', default: 0
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['remember_me_token'], name: 'index_users_on_remember_me_token'
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token'
   end
 
-  add_foreign_key "playlist_songs", "playlists"
-  add_foreign_key "playlist_songs", "songs"
-  add_foreign_key "playlists", "users"
-  add_foreign_key "scores", "songs"
-  add_foreign_key "scores", "users"
+  add_foreign_key 'playlist_songs', 'playlists'
+  add_foreign_key 'playlist_songs', 'songs'
+  add_foreign_key 'playlists', 'users'
+  add_foreign_key 'scores', 'songs'
+  add_foreign_key 'scores', 'users'
 end
